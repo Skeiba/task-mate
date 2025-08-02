ab40045e96bba2e260a78c76b6388f838dbaa842
salah <salah.bahlaouane@gmail.com>
2025-08-02 15:52:12 +0100

    commit this

---
+++ src/main/java/com/salah/taskmate/TaskmateApplication.java
@@ -0,0 +1,12 @@
+package com.salah.taskmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmateApplication.class, args);
+	}
+}
