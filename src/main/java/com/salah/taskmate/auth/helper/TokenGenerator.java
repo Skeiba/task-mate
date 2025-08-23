9bbbbac903e431c2c7b8e4169663f6b204f7a6e7
salah <salah.bahlaouane@gmail.com>
2025-08-23 13:24:00 +0100

    changed the token from a UUID to a random six numbers

--- src/main/java/com/salah/taskmate/auth/helper/TokenGenerator.java
+++ src/main/java/com/salah/taskmate/auth/helper/TokenGenerator.java
@@ -2,11 +2,14 @@ package com.salah.taskmate.auth.helper;
 
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
 import java.util.UUID;
 
 @Component
 public class TokenGenerator {
+    private static final SecureRandom random = new SecureRandom();
+
     public String generateToken() {
-        return UUID.randomUUID().toString();
+        return String.format("%06d", random.nextInt(1_000_000));
     }
 }
